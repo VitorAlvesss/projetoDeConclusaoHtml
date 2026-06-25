f3a09351487964521cde0c24dd3fe161994b0539
Vitor Alves <vitor.asilva52@senacsp.edu.br>
2026-06-25 02:42:41 -0300

    Adicionando verificações de email e de campos nulos na página de fale conosco, além de reduzir linhas por conta do toast.

--- envioDeEmail.js
+++ envioDeEmail.js
@@ -1,8 +1,18 @@
-
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 document.getElementById('formularioContato').addEventListener('submit', function(event){
     event.preventDefault();
     const mudarTextoBotao = document.getElementById('submitButton');
     mudarTextoBotao.textContent = "Enviando...";
+    if(document.getElementById('assunto').value.trim() == "" || document.getElementById('nome').value.trim() == "" || document.getElementById('email').value.trim() == "" 
+    || document.getElementById('mensagem').value.trim() == ""){
+        mensagemNaTela("Preencha todos os campos!")
+        return;
+    }
+
+    if(!regexEmail.test(document.getElementById('email').value)){
+        mensagemNaTela("Email inválido!")
+    }
+
 
     emailjs.send('service_r37mn9j', 'template_qudw1q7', {
        title: document.getElementById('assunto').value,
@@ -11,21 +21,9 @@ document.getElementById('formularioContato').addEventListener('submit', function
        message: document.getElementById('mensagem').value
     })
     .then(() =>{
-        Toastify({
-            text: "E-mail enviado com sucesso!",
-            style: {
-                background: "#28a745",
-                color: "#f4f4f4"
-            },
-        }).showToast();
+        mensagemNaTelaPositiva("Email enviado com sucesso!")
     }, (erro) =>{
-        Toastify({
-            text: "Erro ao enviar email. Tente novamente.",
-            style: {
-                background: "#FF0000",
-                color: "#f4f4f4"
-            },
-        }).showToast();
+        mensagemNaTela("Erro ao enviar email. Tente novamente.")
     })
     .finally(() =>{
         mudarTextoBotao.textContent = "Enviar"
